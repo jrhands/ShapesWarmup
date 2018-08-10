12b6856155918da06c00043ed881400c29c0973e
Jacob Hands <jrhands@gmail.com>
2018-08-10 12:58:03 -0400

    Warmup complete

--- ShapesWarmup/Program.cs
+++ ShapesWarmup/Program.cs
@@ -10,6 +10,13 @@ namespace ShapesWarmup
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle(3, 4);
+            Console.WriteLine(rectangle.ToString());
+            Square square = new Square(5, "Orange");
+            Console.WriteLine(square.ToString());
+            Triangle triangle = new Triangle(7, 4, 5, "Blue");
+            Console.WriteLine(triangle.ToString());
+            Console.Read();
         }
     }
 
@@ -24,19 +31,89 @@ namespace ShapesWarmup
 
         public abstract double CalculateArea();
 
-        
+
     }
 
     public class Rectangle : Shape
     {
-        public Rectangle(int length, int width, string name = "Rectangle") : base(name)
+        public int Length;
+        public int Width;
+
+        public Rectangle(int length, int width) : base("Rectangle")
         {
+            Length = length;
+            Width = width;
+        }
+
+        protected Rectangle(int length, int width, string name) : base(name)
+        {
+            Length = length;
+            Width = width;
         }
 
         public override double CalculateArea()
         {
-            throw new NotImplementedException();
+            return Length * Width;
+        }
+
+        public override string ToString()
+        {
+            return $"This {Name} has area {CalculateArea():N}";
         }
     }
 
+    public interface IColor
+    {
+        string GetColor();
+    }
+
+    public class Square : Rectangle, IColor
+    {
+        public string Color;
+
+        public Square(int sideLength, string color) : base(sideLength, sideLength, "Square")
+        {
+            Color = color;
+        }
+
+        public string GetColor()
+        {
+            return Color;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} and is the color {Color}";
+        }
+    }
+
+    public class Triangle : Shape, IColor
+    {
+        int Side1Length, Side2Length, Side3Length;
+        string Color;
+
+        public Triangle(int side1Length, int side2Length, int side3Length, string color) : base("Triangle")
+        {
+            Side1Length = side1Length;
+            Side2Length = side2Length;
+            Side3Length = side3Length;
+            Color = color;
+        }
+
+        public override double CalculateArea()
+        {
+            double HeronS = (Side1Length + Side2Length + Side3Length) / 2;
+            return Math.Sqrt(HeronS * (HeronS - Side1Length) * (HeronS - Side2Length) * (HeronS - Side3Length));
+        }
+
+        public string GetColor()
+        {
+            return Color;
+        }
+
+        public override string ToString()
+        {
+            return $"This {Name} has area {CalculateArea():N} and is the color {GetColor()}";
+        }
+    }
 }
